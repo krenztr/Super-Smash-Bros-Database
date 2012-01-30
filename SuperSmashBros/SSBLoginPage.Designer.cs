afd0d0f1c9cf962db9f811bae604cb0ac1ca5d62
RC.Z <r.c.cham@gmail.com>
2012-01-29 19:45:24 -0500

    Initial commit

---
+++ SuperSmashBros/SSBLoginPage.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace SuperSmashBros
+{
+    partial class SSBLoginPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enterButton = new System.Windows.Forms.Button();
+            this.backgroundPicture = new System.Windows.Forms.PictureBox();
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.passwordBox = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.registerButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.backgroundPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // enterButton
+            // 
+            this.enterButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.enterButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.enterButton.Font = new System.Drawing.Font("ESSTIXThirteen", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.enterButton.Location = new System.Drawing.Point(649, 550);
+            this.enterButton.Margin = new System.Windows.Forms.Padding(0);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(130, 37);
+            this.enterButton.TabIndex = 3;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = false;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // backgroundPicture
+            // 
+            this.backgroundPicture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.backgroundPicture.BackgroundImage = global::SuperSmashBros.Properties.Resources.brawl_1024;
+            this.backgroundPicture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.backgroundPicture.Location = new System.Drawing.Point(9, 9);
+            this.backgroundPicture.Margin = new System.Windows.Forms.Padding(0);
+            this.backgroundPicture.Name = "backgroundPicture";
+            this.backgroundPicture.Size = new System.Drawing.Size(800, 600);
+            this.backgroundPicture.TabIndex = 0;
+            this.backgroundPicture.TabStop = false;
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.usernameBox.Location = new System.Drawing.Point(649, 490);
+            this.usernameBox.Margin = new System.Windows.Forms.Padding(3, 3, 3, 5);
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.Size = new System.Drawing.Size(130, 22);
+            this.usernameBox.TabIndex = 1;
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.passwordBox.AsciiOnly = true;
+            this.passwordBox.Location = new System.Drawing.Point(649, 520);
+            this.passwordBox.Margin = new System.Windows.Forms.Padding(3, 3, 3, 8);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(130, 22);
+            this.passwordBox.TabIndex = 2;
+            this.passwordBox.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(562, 490);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(562, 520);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(257, 561);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Not a User ?";
+            // 
+            // registerButton
+            // 
+            this.registerButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.registerButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.registerButton.Font = new System.Drawing.Font("Consolas", 10F);
+            this.registerButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.registerButton.Location = new System.Drawing.Point(389, 550);
+            this.registerButton.Margin = new System.Windows.Forms.Padding(0);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(101, 36);
+            this.registerButton.TabIndex = 4;
+            this.registerButton.Text = "Register";
+            this.registerButton.UseVisualStyleBackColor = false;
+            this.registerButton.Click += new System.EventHandler(this.registerButton_Click);
+            // 
+            // SSBLoginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 618);
+            this.Controls.Add(this.registerButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.usernameBox);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.backgroundPicture);
+            this.Name = "SSBLoginPage";
+            this.Text = "SuperSmashBrosDB - Login";
+            ((System.ComponentModel.ISupportInitialize)(this.backgroundPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox backgroundPicture;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.MaskedTextBox passwordBox;
+        private System.Windows.Forms.TextBox usernameBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button registerButton;
+
+    }
+}
+
---
+++ SuperSmashBros/SSBLoginPage.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SuperSmashBros
+{
+    public partial class SSBLoginPage : Form
+    {
+        readonly string pwd = "";
+        public SSBLoginPage()
+        {
+            InitializeComponent();
+            usernameBox.KeyPress += new KeyPressEventHandler(usernameBox_KeyPress);
+
+            passwordBox.KeyDown += new KeyEventHandler(passwordBox_KeyDown);
+            passwordBox.UseSystemPasswordChar = true;
+        }
+
+        private void SSBLoginPage_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        #region Events
+        
+        #region enterButton Event
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            string username = usernameBox.Text;
+            string password = passwordBox.Text;
+
+            string connectionString = "user id=zhangr1;" +
+                                       "Password="+pwd+";" +
+                                       "server=whale.cs.rose-hulman.edu;" +
+                                       "Trusted_Connection=no;" +
+                                       "Database=SuperSmashBros;" +
+                                       "connection timeout=30;" +
+                                       "TrustServerCertificate=true";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                string commandString = "SELECT * FROM PLAYER AS p WHERE p.Username = \'" +
+                    username + "\' AND p.Password = \'" + password + "\'";
+                MessageBox.Show(commandString);
+                SqlDataReader sdr = null;
+                SqlCommand command = new SqlCommand(commandString, connection);
+                sdr = command.ExecuteReader();
+                if (sdr.Read())
+                    MessageBox.Show("Yeah");
+                else
+                    MessageBox.Show("Oops");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                connection.Close();
+                connection.Dispose();
+            }
+        }
+        #endregion
+
+        #region registerButton Event
+        private void registerButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Register Button Clicked");
+        }
+        #endregion
+
+        #region usernameBox Event
+        void usernameBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 127 || e.KeyChar == 8)
+                e.Handled = false;
+            else if (!Char.IsLetterOrDigit(e.KeyChar))
+                e.Handled = true;
+        }
+        #endregion
+
+        #region passwordBox Event
+        void passwordBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                enterButton.PerformClick();
+        }
+        #endregion 
+        
+        #endregion
+    }
+}
